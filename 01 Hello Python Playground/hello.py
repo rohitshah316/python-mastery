92ac4605a291016b890ab3b49e62875e3abb0d60
Rohit Shah <shahrohit2223@gmail.com>
2026-07-27 22:50:14 +0545

    level 1

---
+++ 01 Hello Python Playground/hello.py
@@ -0,0 +1,32 @@
+# # # print("Hello Alex");
+
+# # # print("My name is Rohit Shah.")
+# # language = "Python"
+# # print(language)
+# # print("language")
+
+# # language = "Java"
+# # print(language)
+
+
+# name="Alex"
+# age=20
+# country="Nepal"
+
+# print(f"My name is {name}")
+# print(f"I am {age} years old.")
+# print(f"I live in {country}")
+
+
+# data types: 
+# str-text, int-whole numbers, float-decimal numbers, bool-true/false
+
+name="Alex"  #str
+age=20   #int
+height=5.7 #float
+isStudent=True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(isStudent))
