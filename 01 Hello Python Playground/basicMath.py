1308173392bf394ddeb215fe68afc275778db179
Rohit Shah <shahrohit2223@gmail.com>
2026-07-28 22:16:02 +0545

    level 1

---
+++ 01 Hello Python Playground/basicMath.py
@@ -0,0 +1,12 @@
+
+
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+
+print(f"The sum is {num1+num2}")
+print(f"The diff is {num1-num2}")
+print(f"The product is {num1*num2}")
+print(f"The div is {num1/num2}")
+print(f"The floor div is {num1//num2}")
+print(f"The mod is {num1%num2}")
+print(f"The exponent is {num1**num2}")
--- 01 Hello Python Playground/hello.py
+++ 01 Hello Python Playground/hello.py
@@ -1,32 +1,62 @@
-# # # print("Hello Alex");
+# # # # print("Hello Alex");
 
-# # # print("My name is Rohit Shah.")
-# # language = "Python"
-# # print(language)
-# # print("language")
+# # # # print("My name is Rohit Shah.")
+# # # language = "Python"
+# # # print(language)
+# # # print("language")
 
-# # language = "Java"
-# # print(language)
-
-
-# name="Alex"
-# age=20
-# country="Nepal"
-
-# print(f"My name is {name}")
-# print(f"I am {age} years old.")
-# print(f"I live in {country}")
+# # # language = "Java"
+# # # print(language)
 
 
-# data types: 
-# str-text, int-whole numbers, float-decimal numbers, bool-true/false
+# # name="Alex"
+# # age=20
+# # country="Nepal"
 
-name="Alex"  #str
-age=20   #int
-height=5.7 #float
-isStudent=True
+# # print(f"My name is {name}")
+# # print(f"I am {age} years old.")
+# # print(f"I live in {country}")
+
+
+# # data types: 
+# # str-text, int-whole numbers, float-decimal numbers, bool-true/false
+
+# name="Alex"  #str
+# age=20   #int
+# height=5.7 #float
+# isStudent=True
+
+# print(type(name))
+# print(type(age))
+# print(type(height))
+# print(type(isStudent))
+
+
+
+# name=input("Enter your name:");
+
+# print("Hello ", name)
+
+
+
+# name=input("Enter your name:")
+# country=input("Enter your country:")
+
+# # print("Hello",name)
+# # print("You are from",country)
+# print(f"Hello {name}")
+# print(f"You are from {country}")
+
+
+
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+height=float(input("Enter your height:"))
+
+print(f"Hello {name}")
+print(f"You are {age} years old.")
+print(f"Your height is {height} feet.")
 
 print(type(name))
 print(type(age))
 print(type(height))
-print(type(isStudent))
